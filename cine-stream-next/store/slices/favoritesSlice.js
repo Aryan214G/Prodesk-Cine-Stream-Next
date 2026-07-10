ac5787272058d5e4a7a8611d4742d2af413aa8fb
Aryan214G <aryan214gm@gmail.com>
2026-07-10 22:42:20 +0530

    feat: add favorites slice for Redux store management

---
+++ cine-stream-next/store/slices/favoritesSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const favoritesSlice = createSlice ( {
+
+    name: "favorites",
+
+    initialState: {
+
+        favorites: []
+    },
+
+    reducers: {
+
+
+    }
+})
+
+export default favoritesSlice.reducer;
