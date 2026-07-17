7d96b9d637f746b35b560941ec8a456c97a07120
Aryan <aryan214gm@gmail.com>
2026-07-17 11:41:35 +0530

    test: add initial test for Hello component

---
+++ cine-stream-next/components/sample.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+function Hello() {
+
+    return (
+        <h1>Hello</h1>
+    );
+}
+
+test('renders Hello', () => {
+
+    render(<Hello />)
+
+    expect(
+        screen.getByText("Hello")
+    ).toBeInTheDocument();
+})
